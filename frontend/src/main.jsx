ed07ea0a841ebe2797c3a3643b8c8eb45460cf5b
mrtaz77 <mrtazwar23@gmail.com>
2025-01-15 05:47:04 +0600

    lower case dashboard route and remove comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
