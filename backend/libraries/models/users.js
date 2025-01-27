346b00792f3c58b0a9e7121f7941cc1c7f4cd026
mrtaz77 <mrtazwar23@gmail.com>
2025-01-27 17:20:52 +0600

    Refactor user model move user-related functions to the model and add firebaseUid field

--- backend/libraries/models/users.js
+++ backend/libraries/models/users.js
@@ -15,13 +15,48 @@ const userSchema = new mongoose.Schema(
     picture: { 
       type: String, 
       default: '' 
-    }
-  }, 
-  { 
-    timestamps: true 
+    },
+    firebaseUid: {
+      type: String,
+      required: true,
+      unique: true
+    },
   }
 );
 
 const User = mongoose.model('User', userSchema);
+
+/**
+ * Finds a user by email.
+ * @param {string} email - The email of the user.
+ * @returns {Object} The user object.
+ */
+export const findUserByEmail = async (email) => {
+  return await User.findOne({ email });
+};
+
+/**
+ * Finds a user by username.
+ * @param {string} username - The username of the user.
+ * @returns {Object} The user object.
+ */
+export const findUserByUsername = async (username) => {
+  return await User.findOne({ username });
+}
+
+/**
+ * Creates a new user
+ * @param {Object} userInfo - Contains user information: email, name, picture and uid.
+ */
+export const createUser = async (userInfo) => {
+  const { email, name, picture, uid } = userInfo;
+  const user = new User({
+    email: email,
+    username: name,
+    picture,
+    firebaseUid: uid
+  });
+  await user.save();
+};
 
 export default User;
