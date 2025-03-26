1c9a9069ccfc439dc3052fe95f6bf915dc0821fc
mrtaz77 <mrtazwar23@gmail.com>
2025-03-26 16:38:23 +0600

    feat: add ESLint configuration and update dev dependencies for improved linting and formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  js.configs.recommended,
+  {
+    plugins: {
+      react: pluginReact,
+      prettier: prettier,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "react/react-in-jsx-scope": "off", 
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
