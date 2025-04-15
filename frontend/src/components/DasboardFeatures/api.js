e6fa26b6f5d5e9fabdb82b8118836f6f36ed6b95
3m09 <jahinabrar5@gmail.com>
2025-04-15 07:36:15 +0600

    fixed api call duplication

--- frontend/src/components/DasboardFeatures/api.js
+++ frontend/src/components/DasboardFeatures/api.js
@@ -1,50 +1,6 @@
-import { getAuth,onAuthStateChanged } from "firebase/auth";
+import makeRequest from "@/services/APIcall";
 
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
-
-async function makeRequest(url, method, body) {
-  const auth = getAuth();
-  let data = { status: "error", msg: "" };
-  // Return a promise that resolves when auth state is ready
-  return new Promise((resolve) => {
-    // This listener fires once when auth state is first determined
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      unsubscribe(); // Stop listening immediately after first auth state is determined
-      if (user) {
-        const idToken = await user.getIdToken();
-        const req = {
-          method: method,
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${idToken}`,
-          },
-        }
-        if(body){
-          req.body = JSON.stringify(body);
-        }
-        const response = await fetch(url, req);
-        if (response.ok) {
-          data = await response.json();
-          console.log("res ok from makeRequest");
-          console.log(data);
-        }
-        else if(response.status === 409){
-          data.msg = "overlap";
-          data.res = await response.json();
-          console.log("overlap from makeRequest");
-          console.log(data);
-        }
-        else {
-          console.log("res not ok from makeRequest");
-          data.msg = "Failed to make request";
-        }
-      } else {
-        data.msg = "User not logged in";
-      }
-      resolve(data);
-    });
-  });
-}
 
 const getRecentRecipes = async(noOfRecipes)=>{
   const url = `${API_BASE_URL}/search/history/${noOfRecipes}`;
