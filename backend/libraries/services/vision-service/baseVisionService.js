01bdacf1b794419fd8b5e7c15c806fd60b9cd097
amimulamim <142148987+amimulamim@users.noreply.github.com>
2025-04-19 18:57:08 +0600

    base64Image not used  issue resolved

--- backend/libraries/services/vision-service/baseVisionService.js
+++ backend/libraries/services/vision-service/baseVisionService.js
@@ -1,6 +1,5 @@
 export class BaseVisionService {
-    async analyzeImage(base64Image) {
-      throw new Error("analyzeImage() must be implemented by subclass");
-    }
+  async analyzeImage(_base64Image) {
+    throw new Error("analyzeImage() must be implemented by subclass");
   }
-  
+}
