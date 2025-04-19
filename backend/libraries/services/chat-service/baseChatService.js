3fb4a0a0f107e0c68dfa765e1a71abebb39f59ec
amimulamim <142148987+amimulamim@users.noreply.github.com>
2025-04-19 22:39:23 +0600

    unused param issue addressed

--- backend/libraries/services/chat-service/baseChatService.js
+++ backend/libraries/services/chat-service/baseChatService.js
@@ -1,6 +1,5 @@
 export class BaseChatService {
-    async sendMessage(messages) {
-      throw new Error("sendMessage method not implemented");
-    }
+  async sendMessage(_messages) {
+    throw new Error("sendMessage method not implemented");
+  }
 }
-  
