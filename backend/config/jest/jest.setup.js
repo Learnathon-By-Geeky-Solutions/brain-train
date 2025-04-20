593cff11e3a7bb306b09a1b7ad64001988ebe678
mrtaz77 <mrtazwar23@gmail.com>
2025-04-20 08:06:19 +0600

    update jest setup

---
+++ backend/config/jest/jest.setup.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const globalDataPath = path.resolve(__dirname, "globalSetupData.json");
+
+if (fs.existsSync(globalDataPath)) {
+  const data = JSON.parse(fs.readFileSync(globalDataPath, "utf-8"));
+  global.__TEST_TOKEN__ = data.__TEST_TOKEN__;
+  global.__DISPOSABLE_USER_TOKEN__ = data.__DISPOSABLE_USER_TOKEN__;
+} else {
+  console.error(
+    "❌ globalSetupData.json not found. Make sure globalSetup ran.",
+  );
+}
