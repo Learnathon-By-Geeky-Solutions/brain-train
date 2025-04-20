b35a5bdc0a90e75891e5c9ee8e7b0fcee1a020b6
amimulamim <142148987+amimulamim@users.noreply.github.com>
2025-04-20 15:54:48 +0600

    autocomplete ingr covered corrected

--- backend/apps/search/tests/autoComplete.test.js
+++ backend/apps/search/tests/autoComplete.test.js
@@ -18,6 +18,30 @@ describe("AutoComplete title Endpoint Test", () => {
 
     expect(recipes[0]).toHaveProperty("title");
   });
+  it("should return 200 with recipes suggestions,triggers db,enough result", async () => {
+    const response = await request(app)
+      .get(`${endpoint}?query=ric`)
+      .set("Authorization", `Bearer ${global.__TEST_TOKEN__}`);
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+
+    const recipes = response.body;
+
+    expect(recipes.length).toBeGreaterThan(0);
+    expect(recipes[0]).toHaveProperty("id");
+
+    expect(recipes[0]).toHaveProperty("title");
+  });
+
+  it("should return 500 for no query param", async () => {
+    const response = await request(app)
+      .get(`${endpoint}`)
+      .set("Authorization", `Bearer ${global.__TEST_TOKEN__}`);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("error");
+  });
 });
 
 describe("AutoComplete  Ingr Endpoint Test", () => {
@@ -35,5 +59,13 @@ describe("AutoComplete  Ingr Endpoint Test", () => {
     expect(recipes.length).toBeGreaterThan(0);
 
     expect(recipes[0]).toHaveProperty("name");
+  });
+  it("should return 500 for no query parameter", async () => {
+    const response = await request(app)
+      .get(`${endpoint}`)
+      .set("Authorization", `Bearer ${global.__TEST_TOKEN__}`);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("error");
   });
 });
